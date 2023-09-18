acccb867b7693afb33da5e66df4435931b658df2
Meghana Gundeboina <124271236+meghana010@users.noreply.github.com>
2023-09-18 13:23:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ app.post("/signup", async (req, res) => {
 
 
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
 });
-
